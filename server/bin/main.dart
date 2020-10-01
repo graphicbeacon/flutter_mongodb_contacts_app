fc94a053d9b5c439e0de771beb93740d382e1049
Jermaine Oppong <jeropp00@gmail.com>
2020-10-01 13:40:18 +0100

    Initial commit. Setup folders and dependencies.

---
+++ server/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:contacts_app_server/server.dart' as server;
+
+void main(List<String> arguments) {
+  server.start();
+}
---
+++ server/lib/server.dart
@@ -0,0 +1,6 @@
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:sevr/sevr.dart';
+
+void start() async {
+  //
+}
